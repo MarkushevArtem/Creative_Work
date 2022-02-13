40904d880d1f23c44cac3b0931544d6268eaff79
Markushev Artem <92156536+MarkushevArtem@users.noreply.github.com>
2022-02-13 22:22:11 +0500

    Add files via upload

---
+++ MetodVibora.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+void  printarr(int* a, int c) {
+    for (int i = 0; i < c; i++) {
+        cout << a[i];
+        if (i != c - 1)
+            cout << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    srand(time(0));
+    const int N = 5;
+    int arr[N], y, min;
+    for (int i = 0; i < N; i++)
+        arr[i] = rand() % 100;
+    printarr(arr, N);
+    for (int i = 0; i < N; i++) {
+        min = i;
+        for (int j = i + 1; j < N; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        y = arr[i];
+        arr[i] = arr[min];
+        arr[min] = y;
+    }
+    printarr(arr, N);
+    return 0;
+}
---
+++ StrokaStolbec.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	srand(time(0));
+	int N;
+	cout << "Введите количество строк и столбцов: ";
+	cin >> N;
+	int** arr = new int* [N]; 
+	int k, p, t, z, y, x;
+	for (int i = 0; i < N; i++)
+		arr[i] = new int[N];
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			arr[i][j] = rand() % 100;
+			cout << arr[i][j] << '\t';
+		}
+		cout << endl;
+	}
+	cout << "Введите номер строки и столбца: ";
+	cin >> k >> p;
+	z = arr[k - 1][k - 1];
+	x = arr[k - 1][p - 1];
+	for (int i = 0; i < N; i++)
+	{
+		if (i != k - 1 && i != p - 1)
+		{
+			t = arr[k - 1][i];
+			arr[k - 1][i] = arr[i][p - 1];
+			arr[i][p - 1] = t;
+		}
+		else
+			arr[i][i] = x;
+	}
+	arr[k - 1][p - 1] = z;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			cout << arr[i][j] << '\t';
+		cout << endl;
+	}
+	for (int i = 0; i < N; i++)
+		delete[] arr[i];
+	return 0;
+}
